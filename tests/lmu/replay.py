3e45c8451088665cccd5a4e9de3279122e8d544e
tappi287 <tapper.stefan@gmail.com>
2024-10-24 18:53:25 +0200

    added replay tests

--- tests/lmu/replay.py
+++ tests/lmu/replay.py
@@ -1,6 +1,20 @@
-from lmu.rf2replays import get_replays_location
+from pathlib import Path
+
+from lmu import rf2replays
 
 
 def test_get_replay_folder(set_test_install_location):
-    path = get_replays_location()
-    print(path)
+    path = rf2replays.get_replays_location()
+    assert path == Path(r"C:\py\lmu_settings_widget\tests\data\input\Le Mans Ultimate\UserData\Replays")
+
+
+def test_get_replays(set_test_install_location):
+    replays = rf2replays.get_replays()
+    replay_names = [r["name"] for r in replays]
+
+    assert "Autodromo Nazionale Monza P1 1" in replay_names
+    assert "Bahrain International Circuit R1 1" in replay_names
+
+    assert replays[0]["type"] == 3 and replays[0]["name"] == "Fuji Speedway R1 1"
+    assert replays[1]["type"] == 1 and replays[1]["name"] == "Fuji Speedway Q1 1"
+    assert replays[5]["type"] == 2 and replays[5]["name"] == "Autodromo Nazionale Monza P1 1"
