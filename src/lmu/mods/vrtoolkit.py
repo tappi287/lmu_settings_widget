c9a77d1f04f4880852c41bbcd4f43a3b471ac083
tappi287 <tapper.stefan@gmail.com>
2024-11-05 11:38:34 +0100

    update reshade to 6.3.3

--- src/lmu/mods/vrtoolkit.py
+++ src/lmu/mods/vrtoolkit.py
@@ -10,34 +10,41 @@ from lmu.settingsdef import graphics
 
 
 class VrToolKit:
-    RESHADE_ZIP = 'VRToolkitReshadeUniversal_1.0.1_plus_Clarity.zip'
-    RESHADE_PRESET_DIR = 'reshade-shaders/Presets/'
-    RESHADE_TARGET_PRESET_NAME = 'lmu_widget_preset.ini'
-    RESHADE_INI_NAME = 'ReShadeVR.ini'
+    RESHADE_ZIP = "VRToolkitReshadeUniversal_1.0.2_plus_Clarity.zip"
+    RESHADE_PRESET_DIR = "reshade-shaders/Presets/"
+    RESHADE_TARGET_PRESET_NAME = "lmu_widget_preset.ini"
+    RESHADE_INI_NAME = "ReShadeVR.ini"
 
-    dll_tgt = ('ReShade64.dll', 'dxgi.dll')
+    dll_tgt = ("ReShade64.dll", "dxgi.dll")
     extra_files = [
-        ('rF2_nonPBRmodDay1.png', 'reshade-shaders/Textures'),
-        ('rF2_nonPBRmodDay2.png', 'reshade-shaders/Textures'),
-        ('rF2_ToneDownDay.png', 'reshade-shaders/Textures'),
-        ('lut_ams.png', 'reshade-shaders/Textures'),
-        ('lut_gtr2.png', 'reshade-shaders/Textures'),
-        ('lut_rbr.png', 'reshade-shaders/Textures'),
-        ('lut_filmic_basic.png', 'reshade-shaders/Textures'),
-        ('lut_provia.png', 'reshade-shaders/Textures'),
-        ('lut_technicolor.png', 'reshade-shaders/Textures'),
+        ("rF2_nonPBRmodDay1.png", "reshade-shaders/Textures"),
+        ("rF2_nonPBRmodDay2.png", "reshade-shaders/Textures"),
+        ("rF2_ToneDownDay.png", "reshade-shaders/Textures"),
+        ("lut_ams.png", "reshade-shaders/Textures"),
+        ("lut_gtr2.png", "reshade-shaders/Textures"),
+        ("lut_rbr.png", "reshade-shaders/Textures"),
+        ("lut_filmic_basic.png", "reshade-shaders/Textures"),
+        ("lut_provia.png", "reshade-shaders/Textures"),
+        ("lut_technicolor.png", "reshade-shaders/Textures"),
     ]
-    preprocessor_name = 'PreprocessorDefinitions'
-    preprocessor = {'VRT_SHARPENING_MODE': 0, 'VRT_USE_CENTER_MASK': 0, 'VRT_DITHERING': 0,
-                    'VRT_COLOR_CORRECTION_MODE': 0, 'VRT_ANTIALIASING_MODE': 0, 'LUT_TextureName': '"lut.png"',
-                    'ClarityRGBMode': 0, 'UseClarityDebug': 0}
+    preprocessor_name = "PreprocessorDefinitions"
+    preprocessor = {
+        "VRT_SHARPENING_MODE": 0,
+        "VRT_USE_CENTER_MASK": 0,
+        "VRT_DITHERING": 0,
+        "VRT_COLOR_CORRECTION_MODE": 0,
+        "VRT_ANTIALIASING_MODE": 0,
+        "LUT_TextureName": '"lut.png"',
+        "ClarityRGBMode": 0,
+        "UseClarityDebug": 0,
+    }
 
-    techniques_name = 'Techniques'
-    techniques_sorting = 'TechniqueSorting'
-    vrt_shader = 'VRToolkit.fx'
-    vrt_technique = f'VRToolkit@{vrt_shader}'
-    clarity2_shader = 'Clarity2.fx'
-    clarity2_technique = f'Clarity2@{clarity2_shader}'
+    techniques_name = "Techniques"
+    techniques_sorting = "TechniqueSorting"
+    vrt_shader = "VRToolkit.fx"
+    vrt_technique = f"VRToolkit@{vrt_shader}"
+    clarity2_shader = "Clarity2.fx"
+    clarity2_technique = f"Clarity2@{clarity2_shader}"
 
     def __init__(self, options: Iterator[BaseOptions], location: Path):
         self.options = options
@@ -67,8 +74,8 @@ class VrToolKit:
         self.clarity_ini_keys = set(graphics.reshade_clarity.keys())
 
         for key, setting in settings_dict.items():
-            self.ini_settings[key] = setting.get('value')
-            self.ini_default_settings[key] = setting.get('value')
+            self.ini_settings[key] = setting.get("value")
+            self.ini_default_settings[key] = setting.get("value")
 
     def _update_options(self, update_from_disk=False, clarity_found=True) -> Tuple[bool, bool]:
         use_reshade, use_clarity = False, False
@@ -84,9 +91,9 @@ class VrToolKit:
             for option in preset_options.options:
                 # -- Read from Preset options
                 if not update_from_disk:
-                    if option.key == 'use_reshade':
+                    if option.key == "use_reshade":
                         use_reshade = option.value
-                    elif option.key == 'use_clarity':
+                    elif option.key == "use_clarity":
                         use_clarity = option.value
                     elif option.key in self.preprocessor:
                         self.preprocessor[option.key] = option.value
@@ -94,10 +101,10 @@ class VrToolKit:
                         self.ini_settings[option.key] = option.value
                 # -- Read from disk and update Preset options
                 else:
-                    if option.key == 'use_reshade':
+                    if option.key == "use_reshade":
                         option.value = True
                         option.exists_in_rf = True
-                    if option.key == 'use_clarity':
+                    if option.key == "use_clarity":
                         option.value = clarity_found
                         option.exists_in_rf = True
                     elif option.key in self.preprocessor:
@@ -113,7 +120,7 @@ class VrToolKit:
         reshade_zip = get_data_dir() / self.RESHADE_ZIP
         remove_dirs = list()
 
-        with ZipFile(reshade_zip, 'r') as zip_obj:
+        with ZipFile(reshade_zip, "r") as zip_obj:
             for zip_info in zip_obj.filelist:
                 file = bin_dir / zip_info.filename
 
@@ -123,7 +130,7 @@ class VrToolKit:
                     if file.exists():
                         continue
 
-                    logging.info('Extracting ReShade file %s to rF2 bin dir.', file)
+                    logging.info("Extracting ReShade file %s to rF2 bin dir.", file)
                     zip_obj.extract(zip_info, path=bin_dir)
 
                     # - ReShade64.dll -> dxgi.dll
@@ -133,25 +140,25 @@ class VrToolKit:
                         try:
                             file.rename(new_file)
                         except Exception as e:
-                            logging.error('Could not rename file: %s', e)
+                            logging.error("Could not rename file: %s", e)
                 # -- Remove files found in zip
                 else:
                     # - Remove renamed dll
                     dll_file = Path(file.parent / self.dll_tgt[1])
                     if dll_file.is_file():
                         dll_file.unlink(missing_ok=True)
-                        logging.info('Removing ReShade file %s from rF2 bin dir.', dll_file)
+                        logging.info("Removing ReShade file %s from rF2 bin dir.", dll_file)
 
                     # - Remove files matching zip file
                     if file.is_file():
-                        logging.info('Removing ReShade file %s from rF2 bin dir.', file)
+                        logging.info("Removing ReShade file %s from rF2 bin dir.", file)
                         file.unlink(missing_ok=True)
                     else:
                         if file.exists():
                             remove_dirs.append(file)
 
         # -- Copy/Remove extra files
-        for (file_name, file_target_dir) in self.extra_files:
+        for file_name, file_target_dir in self.extra_files:
             src_file = get_data_dir() / file_name
             target_file = bin_dir / file_target_dir / file_name
 
@@ -163,20 +170,20 @@ class VrToolKit:
                 else:
                     target_file.unlink(missing_ok=True)
             except Exception as e:
-                logging.error('Could not process extra file %s: %s', file_name, e)
+                logging.error("Could not process extra file %s: %s", file_name, e)
 
         return remove_dirs
 
     @staticmethod
     def _add_ini_value_line(key, value):
         if isinstance(value, (str, int)):
-            return f'{key}={value}\n'
+            return f"{key}={value}\n"
         else:
-            return f'{key}={value:.6f}\n'
+            return f"{key}={value:.6f}\n"
 
-    def _update_preset_ini(self, reshade_preset: Path, use_clarity = False):
-        """ Write updated values to custom VRToolKit Preset """
-        p_dict, preprocessor_values = self.preprocessor.copy(), ''
+    def _update_preset_ini(self, reshade_preset: Path, use_clarity=False):
+        """Write updated values to custom VRToolKit Preset"""
+        p_dict, preprocessor_values = self.preprocessor.copy(), ""
 
         # -- Prepare Preprocessor values
         for k, v in p_dict.items():
@@ -186,11 +193,15 @@ class VrToolKit:
         try:
             # -- Create Preset Ini Settings
             configured_preset_lines = list()
-            configured_preset_lines.append(f'{self.preprocessor_name}={preprocessor_values}\n')
-            configured_preset_lines.append(f'{self.techniques_name}={self.vrt_technique}'
-                                           f'{f",{self.clarity2_technique}" if use_clarity else ""}\n')
-            configured_preset_lines.append(f'{self.techniques_sorting}={self.vrt_technique}'
-                                           f'{f",{self.clarity2_technique}" if use_clarity else ""}\n')
+            configured_preset_lines.append(f"{self.preprocessor_name}={preprocessor_values}\n")
+            configured_preset_lines.append(
+                f"{self.techniques_name}={self.vrt_technique}"
+                f'{f",{self.clarity2_technique}" if use_clarity else ""}\n'
+            )
+            configured_preset_lines.append(
+                f"{self.techniques_sorting}={self.vrt_technique}"
+                f'{f",{self.clarity2_technique}" if use_clarity else ""}\n'
+            )
 
             # -- Add [Clarity2.fx] Ini Settings
             clarity_fx_lines = list()
@@ -200,28 +211,28 @@ class VrToolKit:
                 clarity_fx_lines.append(self._add_ini_value_line(k, v))
 
             if use_clarity:
-                configured_preset_lines.append('\n')
-                configured_preset_lines.append(f'[{self.clarity2_shader}]\n')
+                configured_preset_lines.append("\n")
+                configured_preset_lines.append(f"[{self.clarity2_shader}]\n")
                 configured_preset_lines.extend(clarity_fx_lines)
-                configured_preset_lines.append('\n')
+                configured_preset_lines.append("\n")
 
             # -- Add [VRToolkit.fx] Ini Settings
-            configured_preset_lines.append('\n')
-            configured_preset_lines.append(f'[{self.vrt_shader}]\n')
+            configured_preset_lines.append("\n")
+            configured_preset_lines.append(f"[{self.vrt_shader}]\n")
             for k, v in self.ini_settings.items():
                 if k not in self.vr_toolkit_ini_keys or v == self.ini_default_settings.get(k):
                     continue
                 configured_preset_lines.append(self._add_ini_value_line(k, v))
 
             # - Write Preset Ini file
-            logging.info('Creating ReShade Preset file: %s', reshade_preset)
-            with open(reshade_preset, 'w') as f:
+            logging.info("Creating ReShade Preset file: %s", reshade_preset)
+            with open(reshade_preset, "w") as f:
                 f.writelines(configured_preset_lines)
 
             for line in configured_preset_lines:
-                logging.debug('Updated VRToolkit Setting: %s', line.replace('\r', '').replace('\n', ''))
+                logging.debug("Updated VRToolkit Setting: %s", line.replace("\r", "").replace("\n", ""))
         except Exception as e:
-            msg = f'Error writing Reshade Preset Ini file: {e}'
+            msg = f"Error writing Reshade Preset Ini file: {e}"
             logging.error(msg)
             self.error = msg
             return False
@@ -229,28 +240,28 @@ class VrToolKit:
 
     @staticmethod
     def _update_reshade_ini(base_dir: Path):
-        """ update the global reshade preset ini to update preset path """
-        reshade_preset_dir_str = VrToolKit.RESHADE_PRESET_DIR.replace('/', '\\')
+        """update the global reshade preset ini to update preset path"""
+        reshade_preset_dir_str = VrToolKit.RESHADE_PRESET_DIR.replace("/", "\\")
         reshade_preset_path = f".\\{reshade_preset_dir_str}{VrToolKit.RESHADE_TARGET_PRESET_NAME}"
         reshade_ini = base_dir / VrToolKit.RESHADE_INI_NAME
 
         reshade_ini_lines, updated_ini_lines = list(), list()
 
         # -- Read current ReShade.ini
-        with open(reshade_ini, 'r') as f:
+        with open(reshade_ini, "r") as f:
             reshade_ini_lines = f.readlines()
 
         for line in reshade_ini_lines:
-            if line.startswith('PresetPath='):
-                line = f'PresetPath={reshade_preset_path}\n'
+            if line.startswith("PresetPath="):
+                line = f"PresetPath={reshade_preset_path}\n"
             # -- VRToolkit default setting seems to set this to 1 which results in
             #    settings updates not being applied on start up
-            if line.startswith('NoReloadOnInitForNonVR'):
-                line = 'NoReloadOnInitForNonVR=0\n'
+            if line.startswith("NoReloadOnInitForNonVR"):
+                line = "NoReloadOnInitForNonVR=0\n"
             updated_ini_lines.append(line)
 
         # -- Write updated ReShade.ini
-        with open(reshade_ini, 'w') as f:
+        with open(reshade_ini, "w") as f:
             f.writelines(updated_ini_lines)
 
     def write(self):
@@ -261,14 +272,14 @@ class VrToolKit:
         reshade_removed = False
 
         # -- Extract ReShade files
-        logging.info('Applying ReShade settings: %s', use_reshade)
+        logging.info("Applying ReShade settings: %s", use_reshade)
         remove_dirs = self._work_thru_reshade_release_zip(use_reshade, bin_dir)
 
         # -- Remove ReShade directories and files
         if not use_reshade:
             # -- Remove Cache
-            cache = bin_dir / 'ReShade' / 'Cache'
-            for f in cache.glob('*.*'):
+            cache = bin_dir / "ReShade" / "Cache"
+            for f in cache.glob("*.*"):
                 f.unlink()
 
             # -- Remove ReShade preset
@@ -282,7 +293,7 @@ class VrToolKit:
                         d.rmdir()
                     except Exception as e:
                         # Eg. if the use has saved screenshots, directory will not be removed
-                        msg = f'Will not delete ReShade directory with user data: {e}'
+                        msg = f"Will not delete ReShade directory with user data: {e}"
                         logging.error(msg)
                         self.error += msg
 
@@ -295,22 +306,22 @@ class VrToolKit:
         return self._update_preset_ini(reshade_preset, use_clarity)
 
     def _read_preset_ini(self, reshade_preset: Path) -> bool:
-        """ Lookup current VRToolkit Settings on disk """
+        """Lookup current VRToolkit Settings on disk"""
         if not reshade_preset.exists():
             return False
 
         try:
             # - Read Preset Ini file
-            with open(reshade_preset, 'r') as f:
+            with open(reshade_preset, "r") as f:
                 preset_lines = f.readlines()
 
             # - Read settings from Preset Ini file lines
             clarity_found = False
             for line in preset_lines:
                 if line.startswith(self.preprocessor_name):
-                    p_str = line.replace(f'{self.preprocessor_name}=', '').replace('\n', '')
-                    for p_def in p_str.split(','):
-                        key, value = p_def.split('=', 2)
+                    p_str = line.replace(f"{self.preprocessor_name}=", "").replace("\n", "")
+                    for p_def in p_str.split(","):
+                        key, value = p_def.split("=", 2)
                         if key in self.preprocessor:
                             if value.isnumeric():
                                 self.preprocessor[key] = int(value)
@@ -319,13 +330,13 @@ class VrToolKit:
                 if line.startswith(self.techniques_name) and self.clarity2_technique in line:
                     clarity_found = True
                 for k, v in self.ini_settings.items():
-                    splitted_line = line.split('=', 1)
+                    splitted_line = line.split("=", 1)
                     if len(splitted_line) == 2:
-                        line_key, value = line.split('=', 1)
+                        line_key, value = line.split("=", 1)
                     else:
                         continue
                     if line_key == k:
-                        if isinstance(value, str) and value.split('.')[0].isnumeric():
+                        if isinstance(value, str) and value.split(".")[0].isnumeric():
                             # -- Read int
                             if value.isdigit():
                                 self.ini_settings[k] = int(value)
@@ -339,7 +350,7 @@ class VrToolKit:
             # -- Update RfactorPlayer VRToolkit Settings
             self._update_options(update_from_disk=True, clarity_found=clarity_found)
         except Exception as e:
-            msg = f'Error reading Reshade Preset Ini file: {e}'
+            msg = f"Error reading Reshade Preset Ini file: {e}"
             logging.exception(msg, exc_info=e)
             self.error = msg
             return False
