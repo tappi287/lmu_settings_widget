afe46a3bd71affa0783cde760ffb059753dafcc8
tappi287 <tapper.stefan@gmail.com>
2025-07-22 01:31:59 +0200

    add help overlay and related styles to KneeBoard; integrate `KneeBoardHelp` component and enhance layout with fade effect when help is active

--- vue/src/components/pages/KneeBoard.vue
+++ vue/src/components/pages/KneeBoard.vue
@@ -7,75 +7,99 @@
           KneeBoard
         </b-input-group-text>
       </b-input-group-prepend>
+
+      <!-- Spacer -->
+      <div class="form-control bg-transparent no-border" />
+
+      <!-- Preset Selection -->
+      <div class="mt-1">
+        <b-input-group size="sm">
+          <b-input-group-prepend>
+            <b-input-group-text class="bg-transparent no-border title text-white pl-0">
+              <b-link @click="showHelp=true">Help</b-link>
+            </b-input-group-text>
+          </b-input-group-prepend>
+        </b-input-group>
+      </div>
     </b-input-group>
-    <div class="knee-board-content">
-      <div class="info-panel">
-        <b-card class="mt-2 setting-card h-100" header-class="m-0 p-2 text-left"
-                bg-variant="dark" text-variant="white">
-          <template #header>
-            <b-icon icon="tv"></b-icon>
-            <span class="ml-2">Pit Strategy [Placeholder]</span>
-            <slot name="header"></slot>
-          </template>
-          <b-row class="text-left" align-v="center">
-            <b-col>
-              Fuel
-            </b-col>
-            <b-col>
-              <b-progress>
-                <b-progress-bar value="97">
-                  <span>97%</span>
-                </b-progress-bar>
-              </b-progress>
-            </b-col>
-            <b-col>
-              Fuel per Lap Average
-            </b-col>
-            <b-col>
-              <b>2.94%</b>
-            </b-col>
-            <div class="w-100"></div>
-            <b-col>
-              Pit in Lap
-            </b-col>
-            <b-col>
-              16
-            </b-col>
-            <b-col>
-              Time until Empty
-            </b-col>
-            <b-col>
-              02:12:23
-            </b-col>
-          </b-row>
-        </b-card>
+
+    <div class="knee-board-inner-container">
+      <div class="knee-board-content" :class="showHelp ? 'knee-board-content-fade' : ''">
+        <div class="info-panel">
+          <b-card class="mt-2 setting-card h-100" header-class="m-0 p-2 text-left"
+                  bg-variant="dark" text-variant="white">
+            <template #header>
+              <b-icon icon="tv"></b-icon>
+              <span class="ml-2">Pit Strategy [Placeholder]</span>
+              <slot name="header"></slot>
+            </template>
+            <b-row class="text-left" align-v="center">
+              <b-col>
+                Fuel
+              </b-col>
+              <b-col>
+                <b-progress>
+                  <b-progress-bar value="97">
+                    <span>97%</span>
+                  </b-progress-bar>
+                </b-progress>
+              </b-col>
+              <b-col>
+                Fuel per Lap Average
+              </b-col>
+              <b-col>
+                <b>2.94%</b>
+              </b-col>
+              <div class="w-100"></div>
+              <b-col>
+                Pit in Lap
+              </b-col>
+              <b-col>
+                16
+              </b-col>
+              <b-col>
+                Time until Empty
+              </b-col>
+              <b-col>
+                02:12:23
+              </b-col>
+            </b-row>
+          </b-card>
+        </div>
+
+        <!-- Performance Monitor -->
+        <div class="bottom-panel h-panel">
+          <b-card class="mt-2 setting-card panel-card" header-class="m-0 p-2 text-left"
+                  bg-variant="dark" text-variant="white">
+            <template #header>
+              <b-icon icon="pie-chart-fill"></b-icon>
+              <span class="ml-2">Performance</span>
+              <slot name="header"></slot>
+            </template>
+            <PerformanceMonitor />
+          </b-card>
+        </div>
+
+        <!-- Hardware Monitor -->
+        <div class="hardware-panel h-panel">
+          <b-card class="mt-2 setting-card panel-card" header-class="m-0 p-2 text-right"
+                  bg-variant="dark" text-variant="white">
+            <template #header>
+              <span class="mr-2">System</span>
+              <b-icon icon="pie-chart-fill"></b-icon>
+              <slot name="header"></slot>
+            </template>
+            <HardwareInfo :hardware-info="hardwareInfo" />
+          </b-card>
+        </div>
       </div>
 
-      <!-- Performance Monitor -->
-      <div class="bottom-panel h-panel">
-        <b-card class="mt-2 setting-card panel-card" header-class="m-0 p-2 text-left"
-                bg-variant="dark" text-variant="white">
-          <template #header>
-            <b-icon icon="pie-chart-fill"></b-icon>
-            <span class="ml-2">Performance</span>
-            <slot name="header"></slot>
-          </template>
-          <PerformanceMonitor />
-        </b-card>
-      </div>
-
-      <!-- Hardware Monitor -->
-      <div class="hardware-panel h-panel">
-        <b-card class="mt-2 setting-card panel-card" header-class="m-0 p-2 text-right"
-                bg-variant="dark" text-variant="white">
-          <template #header>
-            <span class="mr-2">System</span>
-            <b-icon icon="pie-chart-fill"></b-icon>
-            <slot name="header"></slot>
-          </template>
-          <HardwareInfo :hardware-info="hardwareInfo" />
-        </b-card>
-      </div>
+      <!-- Help Overlay -->
+      <b-overlay :show="showHelp" no-wrap variant="transparent" blur="1px" size="xl">
+        <template v-slot:overlay>
+          <KneeBoardHelp @close="showHelp=false"></KneeBoardHelp>
+        </template>
+      </b-overlay>
     </div>
   </div>
 </template>
@@ -85,10 +109,11 @@ import {getEelJsonObject} from "@/main";
 import lmwLogoUrl from "@/assets/lmw_logo.png";
 import HardwareInfo from "@/components/widgets/HardwareInfo.vue";
 import PerformanceMonitor from "@/components/pages/PerformanceMonitor.vue";
+import KneeBoardHelp from "@/components/pages/KneeBoardHelp.vue";
 
 export default {
   name: 'KneeBoard',
-  components: {PerformanceMonitor, HardwareInfo },
+  components: {KneeBoardHelp, PerformanceMonitor, HardwareInfo },
   props: {live: Boolean},
   data() {
     return {
@@ -114,6 +139,7 @@ export default {
         ram_used_percent: 0
       },
       hardwareUpdateInterval: null,
+      showHelp: true,
     };
   },
   methods: {
---
+++ vue/src/components/pages/KneeBoardHelp.vue
@@ -0,0 +1,71 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "KneeBoardHelp"
+})
+</script>
+
+<template>
+  <b-container class="text-left">
+    <b-card class="mt-2 p-2 setting-card rounded" header-class="m-0 p-2" bg-variant="dark" text-variant="white">
+      <template #header>
+          <h3 class="float-left mr-1">
+            <b-icon icon="clipboard" class="mr-2"></b-icon>KneeBoard
+          </h3>
+          <b-button variant="transparent" @click="$emit('close')" class=" text-white float-right">
+            <b-icon icon="x-circle-fill"></b-icon>
+          </b-button>
+      </template>
+
+      <!-- Description -->
+      <b-card-text>
+        This is a display widget meant to be displayed in OpenKneeBoard in Virtual Reality or on a separate Screen
+        in Pancake mode. You can display performance metrics to easier fine-tune your future Settings while on track.
+      </b-card-text>
+
+      <!-- Actions -->
+      <b-card-text>
+        <b-row class="mt-5">
+          <b-col>
+            <h4>OpenKneeboard</h4>
+            <ol>
+              <li>Install <b-link target="_blank" href="https://go.openkneeboard.com/download">OpenKneeboard</b-link></li>
+              <li>Go to <b class="text-white-50">Settings > Tabs > Add a Tab > Window Capture</b></li>
+              <li>
+                Under <i class="text-white-50">Match window title</i> select <i class="text-white-50">Match with pattern</i> and enter:
+                <pre class="text-white-50">LMU Settings Widget v?.?.?</pre></li>
+            </ol>
+          </b-col>
+          <b-col>
+            <h4>PresentMon</h4>
+            <ol>
+              <li>Install <b-link target="_blank" href="https://game.intel.com/us/intel-presentmon/">Intel® PresentMon</b-link></li>
+              <li>Reboot or make sure PresentMonService is running as a Windows Service</li>
+            </ol>
+          </b-col>
+        </b-row>
+        <!-- Desc -->
+        <b-row>
+          <b-col><span>This will capture this Webbrowser window. You can put this window into fullscreen mode with F11.</span></b-col>
+          <b-col><span>This will install the PresentMon Service which is used to capture performance metrics.</span></b-col>
+        </b-row>
+
+        <!-- App Settings -->
+        <b-row class="mt-4">
+          <b-col>
+            <h4>App Settings</h4>
+            <span>Navigate to <b-link>Preferences Page</b-link> and enable Hardware Metrics.</span>
+          </b-col>
+        </b-row>
+      </b-card-text>
+      <b-card-footer class="text-right p-0">
+        <b-button variant="dark" @click="$emit('close')">Close</b-button>
+      </b-card-footer>
+    </b-card>
+  </b-container>
+</template>
+
+<style scoped>
+
+</style>
