d1fbd20bd521553571470f5d2904d885d655f7eb
tappi287 <tapper.stefan@gmail.com>
2025-07-23 09:34:28 +0200

    update pit strategy placeholder

--- vue/src/components/pages/KneeBoard.vue
+++ vue/src/components/pages/KneeBoard.vue
@@ -25,46 +25,9 @@
 
     <div class="knee-board-inner-container">
       <div class="knee-board-content" :class="showHelp ? 'knee-board-content-fade' : ''">
-        <div class="info-panel">
-          <b-card class="mt-2 setting-card h-100" header-class="m-0 p-2 text-left"
-                  bg-variant="dark" text-variant="white">
-            <template #header>
-              <b-icon icon="tv"></b-icon>
-              <span class="ml-2">Pit Strategy [Placeholder]</span>
-              <slot name="header"></slot>
-            </template>
-            <b-row class="text-left" align-v="center">
-              <b-col>
-                Fuel
-              </b-col>
-              <b-col>
-                <b-progress>
-                  <b-progress-bar value="97">
-                    <span>97%</span>
-                  </b-progress-bar>
-                </b-progress>
-              </b-col>
-              <b-col>
-                Fuel per Lap Average
-              </b-col>
-              <b-col>
-                <b>2.94%</b>
-              </b-col>
-              <div class="w-100"></div>
-              <b-col>
-                Pit in Lap
-              </b-col>
-              <b-col>
-                16
-              </b-col>
-              <b-col>
-                Time until Empty
-              </b-col>
-              <b-col>
-                02:12:23
-              </b-col>
-            </b-row>
-          </b-card>
+        <!-- Placeholder PitStrategy -->
+        <div class="info-panel d-none">
+          <PitStrategy />
         </div>
 
         <!-- Performance Monitor -->
@@ -110,10 +73,11 @@ import lmwLogoUrl from "@/assets/lmw_logo.png";
 import HardwareInfo from "@/components/widgets/HardwareInfo.vue";
 import PerformanceMonitor from "@/components/pages/PerformanceMonitor.vue";
 import KneeBoardHelp from "@/components/pages/KneeBoardHelp.vue";
+import PitStrategy from "@/components/widgets/PitStrategy.vue";
 
 export default {
   name: 'KneeBoard',
-  components: {KneeBoardHelp, PerformanceMonitor, HardwareInfo },
+  components: {KneeBoardHelp, PerformanceMonitor, HardwareInfo, PitStrategy },
   props: {live: Boolean},
   data() {
     return {
---
+++ vue/src/components/widgets/PitStrategy.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: 'HardwareInfo',
+  data() {
+    return {
+      noData: true
+    };
+  }
+}
+</script>
+
+<template>
+  <b-card class="mt-2 setting-card h-100" header-class="m-0 p-2 text-left"
+          bg-variant="dark" text-variant="white">
+    <template #header>
+      <b-icon icon="tv"></b-icon>
+      <span class="ml-2">Pit Strategy [Placeholder]</span>
+      <slot name="header"></slot>
+    </template>
+    <b-row class="text-left" align-v="center">
+      <b-col>
+        Fuel
+      </b-col>
+      <b-col>
+        <b-progress>
+          <b-progress-bar value="97">
+            <span>97%</span>
+          </b-progress-bar>
+        </b-progress>
+      </b-col>
+      <b-col>
+        Fuel per Lap Average
+      </b-col>
+      <b-col>
+        <b>2.94%</b>
+      </b-col>
+      <div class="w-100"></div>
+      <b-col>
+        Pit in Lap
+      </b-col>
+      <b-col>
+        16
+      </b-col>
+      <b-col>
+        Time until Empty
+      </b-col>
+      <b-col>
+        02:12:23
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<style scoped>
+
+</style>
